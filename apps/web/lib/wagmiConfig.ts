8b6372b1864bfd174756793c784e7de44639b63e
mattyoungaviation-collab <mattyoungaviation@gmail.com>
2026-02-11 19:07:06 -0800

    Update wagmiConfig.ts

--- apps/web/lib/wagmiConfig.ts
+++ apps/web/lib/wagmiConfig.ts
@@ -1,6 +1,5 @@
 import { http, createConfig } from 'wagmi';
-import { injected } from '@wagmi/connectors/injected'
-import { walletConnect } from '@wagmi/connectors/walletConnect'
+import { injected, walletConnect } from '@wagmi/connectors';
 import { defineChain } from 'viem';
 
 const ronin = defineChain({
