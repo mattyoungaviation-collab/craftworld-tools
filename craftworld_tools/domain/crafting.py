baef7e75138ae992d1cff432a43ef58759f31e9a
mattyoungaviation-collab <mattyoungaviation@gmail.com>
2026-05-09 02:41:01 -0700

    Point crafting domain wrapper at domain core

--- craftworld_tools/domain/crafting.py
+++ craftworld_tools/domain/crafting.py
@@ -1,9 +1,16 @@
-"""Crafting planner compatibility layer."""
+"""Crafting planner public interface."""
 
-from crafting_planner import (  # noqa: F401
+from .crafting_core import (  # noqa: F401
+    BASE_SYMBOLS,
+    CANONICAL_GRAPH,
     CRAFTING_CHAINS,
+    DEFAULT_MODIFIERS,
     Modifiers,
+    Recipe,
+    RecipeInput,
     build_chain_report,
+    build_recipe_index,
+    get_effective_recipe,
     plan_craft,
     rank_opportunities,
 )
