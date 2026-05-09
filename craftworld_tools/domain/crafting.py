24fd24107e006019e324046e193174945a498523
mattyoungaviation-collab <mattyoungaviation@gmail.com>
2026-05-09 02:29:37 -0700

    Add crafting domain wrapper

---
+++ craftworld_tools/domain/crafting.py
@@ -0,0 +1,9 @@
+"""Crafting planner compatibility layer."""
+
+from crafting_planner import (  # noqa: F401
+    CRAFTING_CHAINS,
+    Modifiers,
+    build_chain_report,
+    plan_craft,
+    rank_opportunities,
+)
