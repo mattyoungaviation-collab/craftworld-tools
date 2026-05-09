cfe527cfba45e269c082c0b6f1c7f382a11124a6
mattyoungaviation-collab <mattyoungaviation@gmail.com>
2026-05-09 02:29:25 -0700

    Add factory domain wrapper

---
+++ craftworld_tools/domain/factories.py
@@ -0,0 +1,15 @@
+"""Factory calculation compatibility layer."""
+
+from factories import (  # noqa: F401
+    FACTORIES_FROM_CSV,
+    FACTORY_DISPLAY_INDEX,
+    FACTORY_DISPLAY_ORDER,
+    MASTERY_BONUSES,
+    WORKSHOP_MODIFIERS,
+    MyFactory,
+    compute_best_setups_csv,
+    compute_factory_result_csv,
+    load_factories_from_csv,
+    my_factories,
+    profit_per_hour,
+)
